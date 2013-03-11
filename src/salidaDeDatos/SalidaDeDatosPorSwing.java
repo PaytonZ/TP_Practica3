9b1cc08ded341c314c47eb0b3998679174594562
paytonZ <jlpv_90@hotmail.com>
2013-03-11 20:27:51 +0100

    Prueba ventana

--- src/salidaDeDatos/SalidaDeDatosPorSwing.java
+++ src/salidaDeDatos/SalidaDeDatosPorSwing.java
@@ -1,16 +1,40 @@
 package salidaDeDatos;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
 
 public class SalidaDeDatosPorSwing {
 
     
-    JFrame marco;
+   
     
+    public void Prueba(){
+	
+	//1. Declarar - definir
+	 JFrame marco=new JFrame("main");
+	    JFrame panel= new JFrame();
+	    JButton boton = new JButton("trollebus.");
+	    
+	    
+	    marco.setLayout(new BorderLayout());
+	    //2. Añadir componentes	    
+	    marco.getLayeredPane().add(boton);
+	 
+	    
+	    //3. Tamaño
+	   marco.pack();
+	    //4. Poner liseners.
+	  
+	    //5. poner visible
+	    marco.setVisible(true);
+	  
+    }
     
     public static void main(String[] args) {
 	
-
+	new SalidaDeDatosPorSwing().Prueba();
     }
 
 }
