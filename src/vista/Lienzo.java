46cd0f6b275cea1190923eb4c322827aa109856f
juancly <juancly@juancly-portatil>
2013-04-26 18:19:48 +0200

    Añadidos colores a la tierra y al cielo del lienzo

--- src/vista/Lienzo.java
+++ src/vista/Lienzo.java
@@ -7,6 +7,8 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.util.Vector;
 
 import javax.swing.JFrame;
@@ -58,16 +60,63 @@ public class Lienzo extends Canvas implements InterfaceEjecuta {
 	int y = Constantes.ALTO_VENTANA / 4;
 
 	int i = 0;
+	
+	 
 	while (x < matriz[matriz.length - 3] / FACTORESCALA) {
+            //pintamos el cielo
+    	    g.setColor(Color.cyan);
+    	    Graphics2D g2d = (Graphics2D)g;
+	    Polygon polygonCielo = new Polygon();
+	    polygonCielo.addPoint(x, 0);
+	    polygonCielo.addPoint(x, y-1);
+	    polygonCielo.addPoint((int) matriz[i + 2] / FACTORESCALA, (y- (int) matriz[i + 1])-1);
+	    polygonCielo.addPoint((int) matriz[i + 2] / FACTORESCALA, 0);
+	    g2d.fillPolygon(polygonCielo);
+	    
+	  //pintamos el suelo
+    	    g.setColor(Color.green);
+	    Polygon polygonSuelo = new Polygon();
+	    polygonSuelo.addPoint(x, Constantes.ALTO_VENTANA);
+	    polygonSuelo.addPoint(x, y+1);
+	    polygonSuelo.addPoint((int) matriz[i + 2] / FACTORESCALA, (y- (int) matriz[i + 1])+1);
+	    polygonSuelo.addPoint((int) matriz[i + 2] / FACTORESCALA, Constantes.ALTO_VENTANA);
+	    g2d.fillPolygon(polygonSuelo);
+	    
+	    //pintamos la carretera
+	    g.setColor(Color.BLACK);
 	    g.drawLine((int) x, y, ((int) matriz[i + 2]) / FACTORESCALA, y
 		    - (int) matriz[i + 1]);
+	    //pintamos la tierra
+	   
+	    
 	    x = ((int) matriz[i + 2]) / FACTORESCALA;
 	    y = y - (int) matriz[i + 1];
 	    i += 2;
 	}
+	//pintamos la ultima parte del cielo
+	g.setColor(Color.cyan);
+	Graphics2D g2d = (Graphics2D)g;
+	Polygon polygonCielo = new Polygon();
+	polygonCielo.addPoint(x, 0);
+	polygonCielo.addPoint(x, y-1);
+	polygonCielo.addPoint((int) matriz[i + 2] / FACTORESCALA,( y- (int) matriz[i + 1])-1);
+	polygonCielo.addPoint((int) matriz[i + 2] / FACTORESCALA, 0);
+	
+	g2d.fillPolygon(polygonCielo);
+	
+	//pintamos la ultima parte del suelo
+	    g.setColor(Color.green);
+	    Polygon polygonSuelo = new Polygon();
+	    polygonSuelo.addPoint(x, Constantes.ALTO_VENTANA);
+	    polygonSuelo.addPoint(x, y+1);
+	    polygonSuelo.addPoint((int) matriz[i + 2] / FACTORESCALA, (y- (int) matriz[i + 1])+1);
+	    polygonSuelo.addPoint((int) matriz[i + 2] / FACTORESCALA, Constantes.ALTO_VENTANA);
+	    g2d.fillPolygon(polygonSuelo);
+	//pintamos la ultima parte de la carretera
+	g.setColor(Color.BLACK);
 	g.drawLine((int) x, y, (int) matriz[matriz.length - 1] / FACTORESCALA,
 		y - (int) matriz[matriz.length - 2]);
-
+	
 	// aqui se pondra la informacion del ciclista para que se vaya pintando,
 	// ahora solo se pinta un punto en pantalla
 	for (i = 0; i < cic.size(); i++) {
