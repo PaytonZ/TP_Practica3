5bd4e4862b33a231d554e537f9a7fd046a59b052
paytonz <usuario_local@PTO0702>
2013-05-20 12:15:28 +0200

    Añadido comando meta.

---
+++ src/comandos/ComandoMeta.java
@@ -0,0 +1,37 @@
+package comandos;
+
+import principal.Presentador;
+
+public class ComandoMeta implements InterfazCommand {
+
+    @Override
+    public void configurarContexto(Presentador presentador) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void execute() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getInformacionInstruccion() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public String obtenerAyuda() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public InterfazCommand parse(String comando) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
